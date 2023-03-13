6e6744c5af2484e3e14965ac804dd7106aaae470
Nacho <iderodrigo@comillas.edu>
2023-03-13 18:59:06 +0100

    Update School Selection
    The architecture expects one script per school document. We change how to select the school (with a Constant Variable) instead of the previous looping.

--- src/create_rec_dataset.py
+++ src/create_rec_dataset.py
@@ -27,7 +27,9 @@ DSET_GROUND_TRUTH_FOLDER = os.path.join(os.getcwd(), "dataset_output", "ground_t
 STUDENTS = 10
 # ACADEMIC_YEARS = ['3º de la ESO', '4º de la ESO', '1º de Bachillerato', '2º de Bachillerato']
 EDUCATION_LEVEL = ["EDUCACIÓN SECUNDARIA OBLIGATORIA"]
-SCHOOLS = ["Recuerdo"]
+# SCHOOL_NICKNAME(s) ara available in 'res/schools/schools_*language*.json'
+# SCHOOL_NICKNAME = "recuerdo"
+SCHOOL_NICKNAME = "recuerdo"
 JUST_TABLE = False
 
 DOCS_Z_FILL = 4
@@ -35,7 +37,6 @@ DOCS_Z_FILL = 4
 
 if __name__ == "__main__":
 
-    # TODO en lugar de pasarle el NAMES_TXT, pásale si quieres nombre o apellidos como una string
     names = sr.load_names(info_to_load="first_names", lang=LANGUAGE)
     family_names = sr.load_names(info_to_load="family_names", lang=LANGUAGE)
     schools_list = sr.load_school_info(lang=LANGUAGE)
@@ -68,156 +69,144 @@ if __name__ == "__main__":
         grades_per_subject_metrics[label] = []
 
     for school in schools_list:
+        if school["nickname"] == SCHOOL_NICKNAME:
+            break
 
-        head_studies = sr.create_head_studies_profile(
+    head_studies = sr.create_head_studies_profile(
+        names=names, family_names=family_names
+    )
+    secretary_name = sr.create_secretary_profile(names=names, family_names=family_names)
+
+    for student in tqdm(range(STUDENTS)):
+
+        pdf_instance = Pdf_records()
+        student_profile, student_name = sr.create_student_profile(
             names=names, family_names=family_names
         )
-        secretary_name = sr.create_secretary_profile(
-            names=names, family_names=family_names
+        pdf_instance.school_setter(school, secretary_name, student_name)
+        pdf_instance.header_features_setter(
+            header_rect=True,
+            header_badge=True,
+            header_title=False,
+            header_info=True,
+            lang=LANGUAGE,
         )
+        pdf_instance.body_features_setter()
+        pdf_instance.random_subjects_setter(copy.deepcopy(subjects_semantic))
+        list_of_dicts = []
 
-        for student in tqdm(range(STUDENTS)):
+        for academic_year in ACADEMIC_YEARS:
 
-            pdf_instance = Pdf_records()
-            student_profile, student_name = sr.create_student_profile(
-                names=names, family_names=family_names
-            )
-            pdf_instance.school_setter(school, secretary_name, student_name)
-            pdf_instance.header_features_setter(
-                header_rect=True,
-                header_badge=True,
-                header_title=False,
-                header_info=True,
-                lang=LANGUAGE,
-            )
-            pdf_instance.body_features_setter()
-            pdf_instance.random_subjects_setter(copy.deepcopy(subjects_semantic))
-            list_of_dicts = []
+            pdf_instance.year_setter(academic_year)
 
-            for academic_year in ACADEMIC_YEARS:
+            extra_info_dict = {"Curso": academic_year}
+            png_dict = {}
 
-                pdf_instance.year_setter(academic_year)
+            pdf_instance.png_dict_setter(png_dict=png_dict)
+            pdf_instance.add_page()
+            pdf_instance.set_font("helvetica", size=14)
+            pdf_instance.move_abscissa(displacement=5)
 
-                extra_info_dict = {"Curso": academic_year}
-                png_dict = {}
-
-                pdf_instance.png_dict_setter(png_dict=png_dict)
-                pdf_instance.add_page()
-                pdf_instance.set_font("helvetica", size=14)
-                pdf_instance.move_abscissa(displacement=5)
-
-                if JUST_TABLE:
-                    grades_per_year = pdf_instance.body_just_table(
-                        subjects_semantic,
-                        x_displacement[student],
-                        y_displacement[student],
-                        alpha_num[student],
-                    )
-
-                else:
-
-                    pdf_instance.adhoc_header()
-                    pdf_instance.move_abscissa(displacement=5)
-                    grades_per_year = pdf_instance.body(
-                        subjects_semantic,
-                        x_displacement[student],
-                        y_displacement[student],
-                        alpha_num[student],
-                        LANGUAGE,
-                    )
-                    # TODO Hardcoded Education Level
-                    # pdf_instance.contextual_information(student_profile, extra_info_dict)
-                    pdf_instance.signature(lang=LANGUAGE, head_studies=head_studies)
-
-                grades_per_subject_metrics = sr.update_grades_per_subject_metrics(
-                    metrics=grades_per_subject_metrics, new_grades=grades_per_year[0]
+            if JUST_TABLE:
+                grades_per_year = pdf_instance.body_just_table(
+                    subjects_semantic,
+                    x_displacement[student],
+                    y_displacement[student],
+                    alpha_num[student],
                 )
-                dictionary = pdf_instance.png_dict_getter()
-                list_of_dicts.append(dictionary)
 
-            pdf_name = os.path.join(
-                PDF_DOCS_FOLDER, "".join([str(student).zfill(DOCS_Z_FILL), ".pdf"])
+            else:
+
+                pdf_instance.adhoc_header()
+                pdf_instance.move_abscissa(displacement=5)
+                grades_per_year = pdf_instance.body(
+                    subjects_semantic,
+                    x_displacement[student],
+                    y_displacement[student],
+                    alpha_num[student],
+                    LANGUAGE,
+                )
+                # TODO Hardcoded Education Level
+                # pdf_instance.contextual_information(student_profile, extra_info_dict)
+                pdf_instance.signature(lang=LANGUAGE, head_studies=head_studies)
+
+            grades_per_subject_metrics = sr.update_grades_per_subject_metrics(
+                metrics=grades_per_subject_metrics, new_grades=grades_per_year[0]
             )
-            pdf_instance.set_meta_data()
+            dictionary = pdf_instance.png_dict_getter()
+            list_of_dicts.append(dictionary)
+
+        pdf_name = os.path.join(
+            PDF_DOCS_FOLDER, "".join([str(student).zfill(DOCS_Z_FILL), ".pdf"])
+        )
+        pdf_instance.set_meta_data()
+
+        try:
+            pdf_instance.output(pdf_name)
+
+        except FileNotFoundError:
+            os.makedirs(PDF_DOCS_FOLDER)
+            pdf_instance.output(pdf_name)
+
+        # Convert '.pdf' to '.png'
+        # TODO is there any other more efficient way?
+        images = convert_from_path(pdf_name)
+
+        pngs_folder = os.path.join(PNG_DOCS_FOLDER, str(student).zfill(DOCS_Z_FILL))
+
+        for i, image in enumerate(images):
+
+            image_copy = np.asarray(image)
+            image_copy = cv2.cvtColor(image_copy, cv2.COLOR_BGR2RGB)
+
+            # show_image(img=image_copy)
+            png_number = str(i).zfill(DOCS_Z_FILL)
+            image_path = os.path.join(pngs_folder, "".join([png_number, ".png"]))
+
+            dict = list_of_dicts[i]
 
             try:
-                pdf_instance.output(pdf_name)
+                image.save(image_path)
+                sr.save_layout(
+                    img=image_copy, img_name=png_number, path=pngs_folder, dict=dict
+                )
+                sr.generate_json(
+                    json_path=pngs_folder, json_number=png_number, dict=dict
+                )
 
             except FileNotFoundError:
-                os.makedirs(PDF_DOCS_FOLDER)
-                pdf_instance.output(pdf_name)
-
-            # Convert '.pdf' to '.png'
-            # TODO is there any other more efficient way?
-            images = convert_from_path(pdf_name)
-
-            pngs_folder = os.path.join(PNG_DOCS_FOLDER, str(student).zfill(DOCS_Z_FILL))
-
-            for i, image in enumerate(images):
-
-                image_copy = np.asarray(image)
-                image_copy = cv2.cvtColor(image_copy, cv2.COLOR_BGR2RGB)
-
-                # show_image(img=image_copy)
-                png_number = str(i).zfill(DOCS_Z_FILL)
-                image_path = os.path.join(pngs_folder, "".join([png_number, ".png"]))
-
-                dict = list_of_dicts[i]
-
-                try:
-                    image.save(image_path)
-                    sr.save_layout(
-                        img=image_copy, img_name=png_number, path=pngs_folder, dict=dict
-                    )
-                    sr.generate_json(
-                        json_path=pngs_folder, json_number=png_number, dict=dict
-                    )
-
-                except FileNotFoundError:
-                    os.makedirs(pngs_folder)
-                    image.save(image_path)
-                    sr.save_layout(
-                        img=image_copy, img_name=png_number, path=pngs_folder, dict=dict
-                    )
-                    sr.generate_json(
-                        json_path=pngs_folder, json_number=png_number, dict=dict
-                    )
-
-                """
-                Also, save in the funsd fashion the '.png' and '.json' (with labels)
-                In addition, save a '.json' with the ground truth
-                """
-
-                student_number = str(student).zfill(DOCS_Z_FILL)
-                id = "".join([student_number, "_", str(i)])
-
-                # Save the image
-                image_path_funsd_fashion = os.path.join(
-                    DSET_IMGS_FOLDER, "".join([id, ".png"])
+                os.makedirs(pngs_folder)
+                image.save(image_path)
+                sr.save_layout(
+                    img=image_copy, img_name=png_number, path=pngs_folder, dict=dict
                 )
-                try:
-                    image.save(image_path_funsd_fashion)
-                except FileNotFoundError:
-                    os.makedirs(DSET_IMGS_FOLDER)
-                    os.makedirs(DSET_ANNOTATIONS_FOLDER)
-                    os.makedirs(DSET_GROUND_TRUTH_FOLDER)
-                    image.save(image_path_funsd_fashion)
-                    # Save the '.json' with labels
-                    sr.generate_json(
-                        json_path=DSET_ANNOTATIONS_FOLDER, json_number=id, dict=dict
-                    )
-                    # Save the '.json' with ground truth
-                    sr.generate_json(
-                        json_path=DSET_GROUND_TRUTH_FOLDER,
-                        json_number=id,
-                        dict=grades_per_year[i],
-                    )
+                sr.generate_json(
+                    json_path=pngs_folder, json_number=png_number, dict=dict
+                )
 
+            """
+            Also, save in the funsd fashion the '.png' and '.json' (with labels)
+            In addition, save a '.json' with the ground truth
+            """
+
+            student_number = str(student).zfill(DOCS_Z_FILL)
+            id = "".join([student_number, "_", str(i)])
+
+            # Save the image
+            image_path_funsd_fashion = os.path.join(
+                DSET_IMGS_FOLDER, "".join([id, ".png"])
+            )
+            try:
+                image.save(image_path_funsd_fashion)
+            except FileNotFoundError:
+                os.makedirs(DSET_IMGS_FOLDER)
+                os.makedirs(DSET_ANNOTATIONS_FOLDER)
+                os.makedirs(DSET_GROUND_TRUTH_FOLDER)
+                image.save(image_path_funsd_fashion)
                 # Save the '.json' with labels
                 sr.generate_json(
                     json_path=DSET_ANNOTATIONS_FOLDER, json_number=id, dict=dict
                 )
-
                 # Save the '.json' with ground truth
                 sr.generate_json(
                     json_path=DSET_GROUND_TRUTH_FOLDER,
@@ -225,8 +214,17 @@ if __name__ == "__main__":
                     dict=grades_per_year[i],
                 )
 
-        # TODO remove this 'break'
-        break
+            # Save the '.json' with labels
+            sr.generate_json(
+                json_path=DSET_ANNOTATIONS_FOLDER, json_number=id, dict=dict
+            )
+
+            # Save the '.json' with ground truth
+            sr.generate_json(
+                json_path=DSET_GROUND_TRUTH_FOLDER,
+                json_number=id,
+                dict=grades_per_year[i],
+            )
 
     dataset_metrics["grades_per_subjects"] = grades_per_subject_metrics
     sr.compute_metrics(metrics=dataset_metrics)
